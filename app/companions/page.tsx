8bae35e3a4f5fed6a64b08e85e9f684c5ee9c29c
DipalokSen <aksharsen68@gmail.com>
2025-06-07 13:15:35 +0600

    implemented Search Functionality

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,10 +1,37 @@
+import { getAllCompanions } from '@/lib/actions/companion.action';
 import React from 'react'
+import CompanionCard from '@/Components/CompanionCard';
+import SearchInput from '@/Components/SearchInput';
+import SubjectFilter from '@/Components/SubjectFilter';
 
-const page = () => {
+const page = async ({searchParams}:SearchParams) => {
+  const filter=await searchParams;
+  const subject=filter.subject?filter.subject:''
+  const topic=filter.topic?filter.topic:''
+ 
+
+const companions=await getAllCompanions({subject,topic});
+  console.log(companions);
+  
   return (
-    <div>
-      company
-    </div>
+    <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="flex gap-4">
+                     <SearchInput />
+                    <SubjectFilter /> 
+                </div>
+            </section>
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                       
+                    />
+                ))}
+            </section>
+        </main>
   )
 }
 
